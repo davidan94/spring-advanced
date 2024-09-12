c14f3b6c5d780d0799d3be5082fce1a0e17cb3e8
Yehwan An <168810508+davidan94@users.noreply.github.com>
2024-09-12 14:50:57 +0900

    level2-5. 테스트 연습 - 1

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -20,7 +20,7 @@ class PasswordEncoderTest {
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
         // when
-        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+        boolean matches = passwordEncoder.matches(rawPassword, encodedPassword); // 5. 테스트 연습 - 1 수정
 
         // then
         assertTrue(matches);
