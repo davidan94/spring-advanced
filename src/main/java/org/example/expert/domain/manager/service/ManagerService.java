256754a0213e6bc84e72a0cf723b9aae60d72f90
Yehwan An <168810508+davidan94@users.noreply.github.com>
2024-09-12 19:34:07 +0900

    level2-8. 유닛 테스트 - 3

--- src/main/java/org/example/expert/domain/manager/service/ManagerService.java
+++ src/main/java/org/example/expert/domain/manager/service/ManagerService.java
@@ -36,6 +36,11 @@ public class ManagerService {
         Todo todo = todoRepository.findById(todoId)
                 .orElseThrow(() -> new InvalidRequestException("Todo not found"));
 
+        // 8. 유닛 테스트 - 3 추가
+        if (todo.getUser() == null) {
+            throw new InvalidRequestException("일정의 작성자가 유효하지 않습니다.");
+        }
+
         if (!ObjectUtils.nullSafeEquals(user.getId(), todo.getUser().getId())) {
             throw new InvalidRequestException("담당자를 등록하려고 하는 유저가 일정을 만든 유저가 유효하지 않습니다.");
         }
